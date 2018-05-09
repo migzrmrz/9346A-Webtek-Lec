1af3c136a88ebdea1ec5e28b57cd8799f22593a9
Janriel Testado <2150048@slu.edu.ph>
2018-05-09 22:46:44 +0800

    Modified and added additional routes to web.php

--- Webtek/routes/web.php
+++ Webtek/routes/web.php
@@ -22,7 +22,16 @@ Route::get('/courses', 'PagesController@courses')->name('courses');
 Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::get('/login', 'PagesController@login')->name('login');
 Route::get('/register', 'PagesController@register')->name('register');
+Route::get('/', 'PagesController@home')->name('home');
+Route::get('/htmlQuiz', 'PagesController@htmlQuiz')->name('htmlQuiz');
+Route::get('/htmlQuestionsPage', 'PagesController@htmlQuestionsPage')->name('htmlQuestions');
+Route::get('/htmlFinal', 'PagesController@htmlFinal')->name('htmlFinal');
+Route::get('/htmlCourse', 'PagesController@htmlCourse')->name('htmlCourse');
+Route::get('/cssCourse', 'PagesController@cssCourse')->name('cssCourse');
+Route::get('/phpCourse', 'PagesController@phpCourse')->name('phpCourse');
+Route::get('/jsCourse', 'PagesController@jsCourse')->name('jsCourse');
+Route::get('/nodeJSCourse', 'PagesController@nodeJSCourse')->name('nodeJSCourse');
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
