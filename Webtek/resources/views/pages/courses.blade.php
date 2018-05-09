e77d9e8712dfe3c9713d5cafa39a7d7ca0fb6508
Janriel Testado <2150048@slu.edu.ph>
2018-05-09 22:45:22 +0800

    Fixed routes in courses.blade.php

--- Webtek/resources/views/pages/courses.blade.php
+++ Webtek/resources/views/pages/courses.blade.php
@@ -9,7 +9,7 @@
 
             <div class="mb-5 element-animate">
               <h1>Level Up Your Skills</h1>
-              <p class="lead">See our courses Below. Learn something new every day with skwela lorem ipsum dolor sit amet.</p>
+              <p class="lead">See our courses Below</p>
               <p><a href="#" class="btn btn-primary">Sign up and get a 7-day free trial</a></p>
             </div>
 
@@ -20,35 +20,34 @@
     </section>
     <!-- END section -->
 
-
     <section class="site-section bg-light">
       <div class="container">
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center">
             <h2>Our Courses</h2>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum magnam illum maiores adipisci pariatur, eveniet.</p>
+            <p class="lead"></p>
           </div>
         </div>
         <div class="row top-course">
           <div class="col-md-4 col-sm-6 col-12">
-            <a href="course-single.html" class="course">
+            <a href="{{ route('htmlCourse')}}" class="course">
               <img src="{{ asset('bootstrap/images/webdesign.jpg') }}" alt="Image placeholder">
-              <h2>Web Design 101</h2>
+              <h2>HTML</h2>
               <p>Enroll Now</p>
             </a>
           </div>
           <div class="col-md-4 col-sm-6 col-12">
-            <a href="course-single.html" class="course">
+            <a href="{{ route('cssCourse')}}" class="course">
               <img src="{{ asset('bootstrap/images/wordpress.jpg') }}" alt="Image placeholder">
-              <h2>Learn How To Develop WordPress Plugin</h2>
+              <h2>CSS</h2>
               <p>Enroll Now</p>
             </a>
           </div>
 
           <div class="col-md-4 col-sm-6 col-12">
-            <a href="course-single.html" class="course">
+            <a href="{{ route('jsCourse')}}" class="course">
               <img src="{{ asset('bootstrap/images/javascript.jpg') }}" alt="Image placeholder">
-              <h2>JavaScript 101</h2>
+              <h2>JavaScript</h2>
               <p>Enroll Now</p>
             </a>
           </div>
@@ -57,39 +56,39 @@
 
         <!-- END row -->
 
-        <div class="row top-course">
+        {{-- <div class="row top-course">
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="course-single.html" class="course">
               <img src="{{ asset('bootstrap/images/webdesign.jpg') }}" alt="Image placeholder">
-              <h2>Web Design 101</h2>
+              <h2>HTML</h2>
               <p>Enroll Now</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="course-single.html" class="course">
               <img src="{{ asset('bootstrap/images/wordpress.jpg') }}" alt="Image placeholder">
-              <h2>Learn How To Develop WordPress Plugin</h2>
+              <h2>CSS</h2>
               <p>Enroll Now</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="course-single.html" class="course">
               <img src="{{ asset('bootstrap/images/javascript.jpg') }}" alt="Image placeholder">
-              <h2>JavaScript 101</h2>
+              <h2>JavaScript</h2>
               <p>Enroll Now</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="course-single.html" class="course">
               <img src="{{ asset('bootstrap/images/photoshop.jpg') }}" alt="Image placeholder">
-              <h2>Photoshop Design 101</h2>
+              <h2>PHP</h2>
               <p>Enroll Now</p>
             </a>
           </div>
           <div class="col-lg-2 col-md-4 col-sm-6 col-12">
             <a href="course-single.html" class="course">
               <img src="{{ asset('bootstrap/images/reactjs.jpg') }}" alt="Image placeholder">
-              <h2>Learn Native ReactJS</h2>
+              <h2>Node JS</h2>
               <p>Enroll Now</p>
             </a>
           </div>
@@ -100,29 +99,22 @@
               <p>Enroll Now</p>
             </a>
           </div>
-        </div>
+        </div> --}}
         <!-- END row -->
 
         
         <div class="row top-course">
           <div class="col-md-4 col-sm-6 col-12">
-            <a href="course-single.html" class="course">
+            <a href="{{ route('phpCourse')}}" class="course">
               <img src="{{ asset('bootstrap/images/photoshop.jpg') }}" alt="Image placeholder">
-              <h2>Photoshop Design 101</h2>
+              <h2>PHP</h2>
               <p>Enroll Now</p>
             </a>
           </div>
           <div class="col-md-4 col-sm-6 col-12">
-            <a href="course-single.html" class="course">
+            <a href="{{ route('nodeJSCourse')}}" class="course">
               <img src="{{ asset('bootstrap/images/reactjs.jpg') }}" alt="Image placeholder">
-              <h2>Learn Native ReactJS</h2>
-              <p>Enroll Now</p>
-            </a>
-          </div>
-          <div class="col-md-4 col-sm-6 col-12">
-            <a href="course-single.html" class="course">
-              <img src="{{ asset('bootstrap/images/angularjs.jpg') }}" alt="Image placeholder">
-              <h2>Learn AngularJS 2</h2>
+              <h2>Node JS</h2>
               <p>Enroll Now</p>
             </a>
           </div>
@@ -228,22 +220,6 @@
       </div>
     </section>
     <!-- END section -->
-
-    <section class="section-cover bg-dark">
-      <div class="container">
-        <div class="row justify-content-center align-items-center intro">
-          <div class="col-md-7 text-center element-animate">
-            <h2>Sign Up And Get a 7-day Free Trial</h2>
-            <p class="lead mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto quidem tempore expedita facere facilis, dolores!</p>
-            <p><a href="#" class="btn btn-primary">Sign up and get a 7-day free trial</a></p>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- END section -->
-
-
-    
   
     <footer class="site-footer" style="background-image: url(bootstrap/images/big_image_3.jpg);">
       <div class="container">
